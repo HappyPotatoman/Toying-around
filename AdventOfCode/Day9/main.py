66e7cc9f4cadbd632e3dcaf905b37b83497e6084
HappyPotatoman <marko.markorupnik@gmail.com>
2022-12-09 10:02:10 +0100

    task 2, minor refactor

--- AdventOfCode/Day9/main.py
+++ AdventOfCode/Day9/main.py
@@ -1,4 +1,3 @@
-
 def move(x, direction):
   if direction == 'R':
     x[0] += 1
@@ -9,42 +8,41 @@ def move(x, direction):
   elif direction == 'D':
     x[1] -= 1
 
-def evaluate_tail_position():
-  if head[0] > tail[0] + 1:
-    move(tail, 'R')
-    if head[1] > tail[1]:
-      move(tail,'U')
-    elif head[1] < tail[1]:
-      move(tail, 'D')
+def evaluate_tail_position(front_knot, back_knot):
+  if front_knot[0] > back_knot[0] + 1:
+    move(back_knot, 'R')
+    if front_knot[1] > back_knot[1]:
+      move(back_knot,'U')
+    elif front_knot[1] < back_knot[1]:
+      move(back_knot, 'D')
 
-  elif head[0] < tail[0] - 1:
-    move(tail, 'L')
-    if head[1] > tail[1]:
-      move(tail, 'U')
-    elif head[1] < tail[1]:
-      move(tail, 'D')
+  elif front_knot[0] < back_knot[0] - 1:
+    move(back_knot, 'L')
+    if front_knot[1] > back_knot[1]:
+      move(back_knot, 'U')
+    elif front_knot[1] < back_knot[1]:
+      move(back_knot, 'D')
   
-  elif head[1] > tail[1] + 1:
-    move(tail, 'U')
-    if head[0] > tail[0]:
-      move(tail, 'R')
-    elif head[0] < tail[0]:
-      move(tail, 'L')
+  elif front_knot[1] > back_knot[1] + 1:
+    move(back_knot, 'U')
+    if front_knot[0] > back_knot[0]:
+      move(back_knot, 'R')
+    elif front_knot[0] < back_knot[0]:
+      move(back_knot, 'L')
   
-  elif head[1] < tail[1] - 1:
-    move(tail, 'D')
-    if head[0] > tail[0]:
-      move(tail, 'R')
-    elif head[0] < tail[0]:
-      move(tail, 'L')
+  elif front_knot[1] < back_knot[1] - 1:
+    move(back_knot, 'D')
+    if front_knot[0] > back_knot[0]:
+      move(back_knot, 'R')
+    elif front_knot[0] < back_knot[0]:
+      move(back_knot, 'L')
 
   # add tail position to set
-  if (tail[0], tail[1]) not in tail_trail: 
-    tail_trail.add((tail[0], tail[1]))
+  if (head[-1][0], head[-1][1]) not in tail_trail: 
+    tail_trail.add((head[-1][0], head[-1][1]))
 
 # Starting position
-head = [0,0]
-tail = [0,0]
+head = [[0,0] for _ in range(10)]
 tail_trail = set()
 
 with open('input.txt') as f:
@@ -52,7 +50,8 @@ with open('input.txt') as f:
     l = line.rstrip().split(" ")
     direction, steps = l[0], int(l[1])
     for i in range(steps):
-      move(head, direction)
-      evaluate_tail_position()
+      move(head[0], direction)
+      for x in range(len(head)-1):
+        evaluate_tail_position(head[x], head[x+1])
 
 print(f'The tail of the rope has visited: {len(tail_trail)} unique positions.')
