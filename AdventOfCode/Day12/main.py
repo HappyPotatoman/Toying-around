4ecf9b822a476cdb5f282296102534d7d4f24d35
HappyPotatoman <marko.markorupnik@gmail.com>
2022-12-12 19:41:32 +0100

    task2, minor refactor

--- AdventOfCode/Day12/main.py
+++ AdventOfCode/Day12/main.py
@@ -14,22 +14,28 @@ def read(input, output):
 
 def main():
   directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-  matrix, start = read('input.txt', [])
+  matrix, _ = read('input.txt', [])
   m, n = len(matrix), len(matrix[0])
-  q = [(start, 1)]
-  visited = set()
-  while q:
-    coords, s = q.pop(0)
-    r, c = coords
-    currentElevation = matrix[r][c]
-    for dr, dc in directions:
-      nr, nc = r + dr, c + dc
-      if 0 <= nr < m and 0 <= nc < n and matrix[nr][nc] == 'E' and (currentElevation == 'z' or currentElevation == 'y'):
-        print(s)
-        return
-      elif 0 <= nr < m and 0 <= nc < n and ord(matrix[nr][nc]) <= ord(currentElevation) + 1 and (nr, nc) not in visited:
-        visited.add((nr, nc))
-        q.append(((nr, nc), s + 1))
+  possibleRoutes = []
+  # BFS for each possible starting position 'a'
+  for i in range(m):
+    for j in range(n):
+      visited = set()
+      if matrix[i][j] == 'a':
+        q = [((i, j), 1)] # initialize queue as (coords, steps)
+        while q:
+          coords, s = q.pop(0)
+          r, c = coords
+          currentElevation = matrix[r][c]
+          for dr, dc in directions:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < m and 0 <= nc < n and matrix[nr][nc] == 'E' and (currentElevation == 'z' or currentElevation == 'y'):
+              possibleRoutes.append(s)
+            elif 0 <= nr < m and 0 <= nc < n and ord(matrix[nr][nc]) <= ord(currentElevation) + 1 and (nr, nc) not in visited:
+              visited.add((nr, nc))
+              q.append(((nr, nc), s + 1))
+
+  print(min(possibleRoutes))
       
 if __name__ == '__main__':
   main()
