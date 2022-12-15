40e2888d8d2be9debfbe72fe6d2d72848aa6d647
HappyPotatoman <marko.markorupnik@gmail.com>
2022-12-15 16:31:43 +0100

    task 2

--- AdventOfCode/Day14/main.py
+++ AdventOfCode/Day14/main.py
@@ -1,8 +1,9 @@
 from itertools import pairwise
 
+task = 2
 data = []
 x_min, x_max, y_max = float('inf'), 0, 0
-with open('input.txt') as f:
+with open('test.txt') as f:
   for line in f:
     l = line.rstrip().split(' -> ')
     l = [l.split(',') for l in l]
@@ -19,7 +20,13 @@ with open('input.txt') as f:
         y_max = y
         
   # normalize matrix size in regard to boundaries
-  matrix = [['.' for i in range((x_max - x_min + 1))] for j in range(y_max + 1)]
+  if task != 2:
+    tilt = 0
+    matrix = [['.' for i in range((x_max - x_min + 1))] for j in range(y_max + 1)]
+  else:
+    # this is an arbitrary value to make the matrix wider, could be improved
+    tilt = 20
+    matrix = [['.' for i in range((x_max - x_min + 1) + tilt * 2)] for j in range(y_max + 1 + 2)]
   
 
   # insert rock positions
@@ -29,25 +36,31 @@ with open('input.txt') as f:
       if xi == xj:
         if yi <= yj:
           for y in range(yi, yj + 1):
-            matrix[y][xi - x_min] = '#'
+            matrix[y][xi - x_min + tilt] = '#'
         else:
           for y in range(yi, yj - 1, -1):
-            matrix[y][xi - x_min] = '#'
+            matrix[y][xi - x_min + tilt] = '#'
       if yi == yj:
         if xi <= xj:
           for x in range(xi, xj + 1):
-            matrix[yi][x - x_min] = '#'
+            matrix[yi][x - x_min + tilt] = '#'
         else:
           for x in range(xi, xj - 1, -1):
-            matrix[yi][x - x_min] = '#'
+            matrix[yi][x - x_min + tilt] = '#'
+
+  if task == 2:
+    for i, _ in enumerate(matrix[-1]):
+      matrix[-1][i] = '#'
 
 
 
 m, n = len(matrix), len(matrix[0])
 count = 0
-def pouringSand(x, y):
-  if 0 > x or x >= m - 1 or 0 > y or y >= n - 1:
+def pouringSand(x, y, task = 1):
+  if 0 > x or x >= m - 1 or 0 > y or y >= n - 1 and task == 1:
     return 'OOB'
+  if x == 0 and matrix[x][y] == 'o':
+    return 'FULL'
   elif matrix[x + 1][y] == '.':
     pouringSand(x + 1, y)
   elif matrix[x + 1][y] != '.' and matrix[x + 1][y + 1] != '.' and matrix[x + 1][y-1] != '.':
@@ -59,11 +72,14 @@ def pouringSand(x, y):
   elif matrix[x + 1][y] != '.' and matrix[x + 1][y + 1] == '.':
     pouringSand(x + 1, y + 1)
 
-for _ in range(10000):
-  if pouringSand(0, 500 - x_min) == 'OOB':
-    break
+run = True
+while run:
+  if pouringSand(0, 500 - x_min + tilt) == 'OOB':
+    run = False
+  elif pouringSand(0, 500 - x_min + tilt) == 'FULL':
+    run = False
   else:
-    pouringSand(0, 500 - x_min)
+    pouringSand(0, 500 - x_min + tilt)
 
 for row in matrix:
   print(''.join(row))
